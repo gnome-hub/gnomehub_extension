7e2c4abd2b3408c9346580d41cb32b85257bc40b
Colton Kammes <ckammes@nd.edu>
2022-01-18 12:04:58 -0500

    added a basic working template for reference

--- gnomehub@gnome-hub.github.io/extension.js
+++ gnomehub@gnome-hub.github.io/extension.js
@@ -1,34 +1,50 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
+const St = imports.gi.St;
+const Gio = imports.gi.Gio;
 
-/* exported init */
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
 
 class Extension {
     constructor() {
+        this._indicator = null;
     }
-
+    
     enable() {
-    }
+        log(`enabling ${Me.metadata.name}`);
 
+        let indicatorName = `${Me.metadata.name} Indicator`;
+        
+        // Create a panel button
+        this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
+        
+        // Add an icon
+        let icon = new St.Icon({
+            gicon: new Gio.ThemedIcon({name: 'face-laugh-symbolic'}),
+            style_class: 'system-status-icon'
+        });
+        this._indicator.add_child(icon);
+
+        // `Main.panel` is the actual panel you see at the top of the screen,
+        // not a class constructor.
+        Main.panel.addToStatusArea(indicatorName, this._indicator);
+    }
+    
+    // REMINDER: It's required for extensions to clean up after themselves when
+    // they are disabled. This is required for approval during review!
     disable() {
+        log(`disabling ${Me.metadata.name}`);
+
+        this._indicator.destroy();
+        this._indicator = null;
     }
 }
 
+
 function init() {
+    log(`initializing ${Me.metadata.name}`);
+    
     return new Extension();
 }
--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# copy extension to extensions folder and enable it
 cp -r gnomehub@gnome-hub.github.io ~/.local/share/gnome-shell/extensions/
+gnome-extensions enable gnomehub@gnome-hub.github.io
+
+# restart gnome shell to reload the extensions
 killall -SIGQUIT gnome-shell
 
