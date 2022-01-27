d57c438a5387fa3021cf97533b550a10edaa8c16
Colton Kammes <ckammes@nd.edu>
2022-01-26 19:48:33 -0500

    figured out libraries and created a placeholder extension dropdown box

--- gnomehub@gnome-hub.github.io/extension.js
+++ gnomehub@gnome-hub.github.io/extension.js
@@ -1,24 +1,70 @@
-const St = imports.gi.St;
-const Gio = imports.gi.Gio;
+const { GObject, St, Clutter, GLib, Gio } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
+const Dropdown = GObject.registerClass(
+    class Indicator extends PanelMenu.Button {
+        _init() {
+            super._init(0.0, 'gnome-hub');
+            log("gnomehub: in indicator")
+
+            // can choose between icon or label
+            this._label = new St.Label({
+                'y_align' : Clutter.ActorAlign.CENTER,
+                'text': 'gnomehub_placeholder',
+                'style_class': 'label'
+            });
+
+            this.add_child(this._label);
+
+            // Add an icon 
+            /*
+            this._icon = new St.Icon({
+                gicon: new Gio.ThemedIcon({name: 'face-laugh-symbolic'}),
+                style_class: 'system-status-icon'
+            });
+            this.add_child(this._icon);
+            */
+
+            // do for each item we want in the menu
+            let settingsMenuItem = new PopupMenu.PopupMenuItem('Settings');
+            settingsMenuItem.connect('activate', () => {
+                ExtensionUtils.openPrefs();
+            });
+            this.menu.addMenuItem(settingsMenuItem); 
+        }
+
+        setText(text) {
+            return this._label.set_text(text);
+        }
+    }
+)
 
 class Extension {
-    constructor() {
+    constructor(uuid) {
         this._indicator = null;
+        this._uui = uuid
+        log("gnomehub: In constructor")
     }
-    
+    enable() {
+        this._indicator = new Dropdown();
+        log("gnomehub: In enable")
+        Main.panel.addToStatusArea(this._uuid, this._indicator, 0, 'right');
+    }
+
+/*
     enable() {
         log(`enabling ${Me.metadata.name}`);
 
         let indicatorName = `${Me.metadata.name} Indicator`;
         
         // Create a panel button
-        this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
+        //this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
+        this._indicator = new Dropdown();
         
         // Add an icon
         let icon = new St.Icon({
@@ -31,6 +77,7 @@ class Extension {
         // not a class constructor.
         Main.panel.addToStatusArea(indicatorName, this._indicator);
     }
+*/
     
     // REMINDER: It's required for extensions to clean up after themselves when
     // they are disabled. This is required for approval during review!
@@ -42,9 +89,37 @@ class Extension {
     }
 }
 
-
 function init() {
     log(`initializing ${Me.metadata.name}`);
     
     return new Extension();
 }
+
+
+/*
+const {St, Clutter} = imports.gi;
+const Main = imports.ui.main;
+
+let panelButton;
+
+function init () {
+    panelButton = new St.Bin({
+        style_class : "panel-button",
+    });
+
+    let panelButtonText = new St.label({
+        text : "Hello World",
+        y_align: Clutter.ActorAlign.CENTER,
+    });
+
+    panelButton.set_child(panelButtonText);
+}
+
+function enable () {
+    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
+}
+
+function disable () {
+    Main.panel._rightBox.remove_child(panelButton);
+}
+*/
